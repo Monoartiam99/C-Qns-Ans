805b92bb4255c0a7f31bf5781638a17ef168fdb7
Monoartiam99 <nandymonojitcourse99.24@gmail.com>
2025-01-20 00:24:59 +0530

    Factorial Using Recursion

--- Day-6.17.c
+++ Day-6.17.c
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #include <stdio.h>
 
 // Function to calculate factorial using recursion.
@@ -23,29 +23,3 @@ int main() {
 
     return 0;
 }
-=======
-#include <stdio.h>
-
-// Function to calculate factorial using recursion.
-int factorial(int n) {
-    if (n == 0) {
-        return 1;
-    } else {
-        return n * factorial(n - 1);
-    }
-}
-
-int main() {
-    int n;
-    printf("Enter a number: ");
-    scanf("%d", &n);
-
-    // Calculate factorial
-    int result = factorial(n);
-
-    // Print the result
-    printf("Factorial of %d is %d\n", n, result);
-
-    return 0;
-}
->>>>>>> fef790d45792d805e8077c047d1d2df6abebc11b
